3341b181ee3659473f26c3fe98c0e456779bfa3d
Isabela Silva <isabela.vsilva@bandtec.com.br>
2025-07-23 12:00:05 -0300

    Initialize the project

---
+++ src/main/java/br/com/Isabela01vSilva/schedulo/ScheduloApplication.java
@@ -0,0 +1,13 @@
+package br.com.Isabela01vSilva.schedulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/Isabela01vSilva/schedulo/ScheduloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Isabela01vSilva.schedulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
