73e000d19467763bf361a1e74b9825d4130fe0f5
Isabela Silva <isabela.vsilva@bandtec.com.br>
2025-08-13 14:51:05 -0300

    feat: validate appointment date

--- src/main/java/br/com/Isabela01vSilva/schedulo/service/CreateAppointmentService.java
+++ src/main/java/br/com/Isabela01vSilva/schedulo/service/CreateAppointmentService.java
@@ -7,6 +7,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 public class CreateAppointmentService {
 
@@ -15,11 +17,23 @@ public class CreateAppointmentService {
 
     @Transactional
     public void createAppointment(CreateAppointmentRequest request){
+
+        validateDate(request.executionDate());
+
         Appointment appointment = new Appointment();
         appointment.setAppName(request.appName());
         appointment.setExecutionDate(request.executionDate());
         appointment.setPayload(request.payload());
         appointment.setStatus(request.status());
+
         appointmentRepository.save(appointment);
+    }
+
+    public void validateDate(LocalDate data){
+        LocalDate today = LocalDate.now();
+
+        if (data.isBefore(today)){
+            throw new IllegalArgumentException("The appointment data cannot be in the past");
+        }
     }
 }
